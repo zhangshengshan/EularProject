9a269bdbb151723a6d4cb887f37f7b0df440d152
zhangshengshan <zsszsszsszss@126.com>
2016-07-28 15:28:05 +0800

    change

--- src/main/scala/Eular2.scala
+++ src/main/scala/Eular2.scala
@@ -2,20 +2,28 @@
   * Created by zhangshengshan on 16/7/28.
   */
 
+
 object Eular2 {
 
-  def fabnaci(n:Int):Int = n match {
+  def fabnaci(n: Int): Int = n match {
     case 1 => 1
     case 2 => 2
-    case _ => fabnaci(n-1)+fabnaci(n-2)
+    case _ => fabnaci(n - 1) + fabnaci(n - 2)
   }
 
 
   def main(args: Array[String]): Unit = {
     var n = 1
-    while(fabnaci(n)<4000000){
+    var sum = 0
+    var temp = 0
+    while (fabnaci(n) < 4000000) {
       println(fabnaci(n))
-      n=n+1
+
+      temp = if (fabnaci(n)%2==0) fabnaci(n) else 0
+      sum = sum + temp
+
+      n = n + 1
     }
+    println("sum is "+sum)
   }
 }
