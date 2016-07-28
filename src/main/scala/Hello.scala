17c37137f387af4bf75100e949174b3cab51c24d
zhangshengshan <zsszsszsszss@126.com>
2016-07-28 13:49:08 +0800

    change

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,8 +1,7 @@
 object Hello {
-  def mul_3_5[A](x:Int):Boolean = (x % 3 == 0) || (x % 5 == 0)
 
   def main(args: Array[String]): Unit = {
-    val sum = List.range(1,999).filter(mul_3_5).reduce( _ +  _ )
+    val sum = List.range(1,999).filter((x:Int) => (x%3==0)||(x%5==0)).reduce( _ +  _ )
     println("sum is "+sum)
   }
 }
